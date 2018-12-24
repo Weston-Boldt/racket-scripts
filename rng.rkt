c05bf89243475bf531f26754cc0cbdb34ddb78ef
weston <boldt.weston@gmail.com>
2018-12-24 00:29:44 -0600

    rng.rkt code-cleanup

--- rng.rkt
+++ rng.rkt
@@ -4,12 +4,13 @@
 (define (get-int x args)
     (string->number (list-ref args x)))
 
-(define (get-random-number)
-    (define args (vector->list (current-command-line-arguments)))
+(define args (vector->list (current-command-line-arguments)))
+
+(define (get-random-number cli-args)
     (if (pair? args)
         (if (> (length args) 1)
             (random (get-int 0 args) (get-int 1 args))
             (random 0 (get-int 0 args)))
         (random 0 100)))
 
-(get-random-number)
+(get-random-number args)
